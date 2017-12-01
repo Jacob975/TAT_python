c3b3e010fb126ceb029dd639a699ce36496e2430
jacob975 <z123a123s123@gmail.com>
2017-12-02 00:41:47 +0800

    This is the new constants table.

--- TAT_env.py
+++ TAT_env.py
@@ -41,3 +41,21 @@ path_of_code = "/home/Jacob975/bin/tat_python"
 # result path will be where to save.
 # recommand: /home/tat_result
 path_of_result = "/home/Jacob975/demo"
+
+#--------------- Object list---------------------------------
+# every object recorded below will be read
+object_list = []
+object_list.append({'RA':'21:53:24','DEC':'47:16:00','name':'IC5146'})
+object_list.append({'RA':'03:29:10','DEC':'31:21:57','name':'NGC1333A'})
+object_list.append({'RA':'12:55:38','DEC':'25:53:31','name':'WD1253+261'})
+object_list.append({'RA':'18:36:57','DEC':'-28:55:42','name':'SgrNova'})
+object_list.append({'RA':'19:20:30','DEC':'11:02:01','name':'HH32'})
+object_list.append({'RA':'08:22:27','DEC':'13:44:07','name':'KELT-17'})
+object_list.append({'RA':'11:52:58.8','DEC':'37:43:07.2','name':'Groombridge1830'})
+object_list.append({'RA':'20:06:15', 'DEC':'44:27:24', 'name': 'KIC8462852'})
+object_list.append({'RA':'21:29:58.42','DEC':'51:03:59.8','name':'PN'})
+object_list.append({'RA':'21:06:53.9','DEC':'38:44:57.9','name':'61Cygni'})
+object_list.append({'RA':'20:12:7', 'DEC':'38:21:18', 'name':'NGC6888'})
+
+#--------------- band list-------------------------
+band_list = ["A", "B", "C", "N", "R", "V" ]
