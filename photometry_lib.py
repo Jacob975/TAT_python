75f989207ecb1512c9a9bc1b24ef09056186eade
jacob975 <z123a123s123@gmail.com>
2018-09-25 12:58:31 +0000

    introduce stdev into IDP

--- photometry_lib.py
+++ photometry_lib.py
@@ -21,6 +21,7 @@ import numpy as np
 import time
 from scipy import optimize
 from cataio_lib import get_catalog
+from uncertainties import ufloat, unumpy
 
 class ensemble_photometry():
     # The demo of data structure.
@@ -111,3 +112,66 @@ class IDP():
         airmass = paras[0] * np.power(source[:,0] - paras[1], 2) + paras[2]
         airmass_curve = np.transpose([source[:,0], airmass])
         return airmass_curve
+
+# The improve differential photometry with error consideration.
+class IDP_w_stdev():
+    # The demo of data structure.
+    #   A target = [[ t1, flux1, stdev],
+    #               [ t2, flux2, stdev]
+    #               [ t3, flux3, stdev],
+    #               ...
+    #              ]
+    #   comparison_star = [[ t1, flux1, stdev],
+    #                      [ t2, flux2, stdev]
+    #                      [ t3, flux3, stdev],
+    #                      ...
+    #                     ]
+    #   auxilary_stars = [star1, star2, star3 ....]
+    #   auxiliary_star = [[ t1, flux1, stdev],
+    #                     [ t2, flux2, stdev]
+    #                     [ t3, flux3, stdev],
+    #                     ...
+    #                    ]
+    def __init__(self, target, comparison_star, auxiliary_stars):
+        self.target = target
+        self.comparison_star = comparison_star
+        self.auxiliary_stars = auxiliary_stars
+    def do(self):
+        #-----------------------------------
+        # Fit airmass of the comparison star
+        comparison_airmass, comparison_aml, comparison_fluctuation = self.fit_airmass(self.comparison_star)
+        #-----------------------------------
+        aux_stars_aml = []
+        fluctuations = []
+        # All auxiliary_stars are divided by themself airmass function.
+        for i in range(len(self.auxiliary_stars)):
+            aux_airmass, aux_star_aml, fluctuation = self.fit_airmass(self.auxiliary_stars[i])
+            aux_stars_aml.append(aux_star_aml)
+            fluctuations.append(fluctuation)
+        # mean the flux of auxiliary_stars_aml
+        aux_stars_aml = np.array(aux_stars_aml)
+        mean_aux = np.average(aux_stars_aml, axis = 0, weights = fluctuations)
+        #----------------------------------------------
+        # divide the target with airmass of comparison_star and the mean of auiliary_stars_aml
+        u_target = unumpy.uarray(self.target[:,1], self.target[:,2])
+        u_comparison_airmass = unumpy.uarray(comparison_airmass[:,1], comparison_airmass[:,2])
+        target_aml = u_target/u_comparison_airmass
+        u_mean_aux = unumpy.uarray(mean_aux[:,1], mean_aux[:,2])
+        target_idp = target_aml/u_mean_aux
+        target_idp_curve = np.transpose([self.target[:,0], unumpy.nominal_values(target_idp), unumpy.std_devs(target_idp)])
+        return target_idp_curve, comparison_airmass, mean_aux 
+    # Assume airmass is a 2d parabola curve, fit the data with this function.
+    def fit_airmass(self, source):
+        paras, cov = optimize.curve_fit(parabola, source[:,0], source[:,1])
+        a = ufloat(paras[0], np.sqrt(cov[0][0]))
+        b = ufloat(paras[1], np.sqrt(cov[1][1]))
+        c = ufloat(paras[2], np.sqrt(cov[2][2]))
+        print a, b, c
+        u_source = unumpy.uarray(source[:,1], source[:,2])
+        u_airmass = a * ((source[:,0] - b)**2) + c
+        print u_airmass
+        airmass_curve = np.transpose([source[:,0], unumpy.nominal_values(u_airmass), unumpy.std_devs(u_airmass)], )
+        u_aml = u_source/u_airmass
+        aml_curve = np.transpose([source[:,0], unumpy.nominal_values(u_aml), unumpy.std_devs(u_aml)])
+        fluctuation = np.sum(np.power(aml_curve[:,1] - np.mean(aml_curve[:,1]),2))/(len(source) -1)
+        return airmass_curve, aml_curve, fluctuation
--- test_IDP.py
+++ test_IDP.py
@@ -12,7 +12,7 @@ update log
 '''
 import numpy as np
 import time
-from photometry_lib import parabola, IDP
+from photometry_lib import parabola, IDP, IDP_w_stdev
 import matplotlib.pyplot as plt
 
 #--------------------------------------------
@@ -29,13 +29,23 @@ if __name__ == "__main__":
     airmass = parabola(t, -0.3, 50, 1000)
     # sine wave (radian)
     intrinsic = np.sin(2 * np.pi * t / T) + 2
-    target = np.transpose([t, airmass * intrinsic + np.random.normal(0, stdev, 100)])
-    comparison_star = np.transpose([t, airmass + np.random.normal(0, stdev, 100)])
-    auxiliary_star1 = np.transpose([t, airmass + np.random.normal(0, stdev, 100)])
-    auxiliary_star2 = np.transpose([t, airmass + np.random.normal(0, stdev, 100)])
-    auxiliary_star3 = np.transpose([t, airmass + np.random.normal(0, stdev, 100)])
+    flux = airmass * intrinsic + np.random.normal(0, stdev, 100)
+    target = np.transpose([ t, flux, np.sqrt(flux)])
+    
+    flux_comparison = airmass + np.random.normal(0, stdev, 100)
+    comparison_star = np.transpose([t, flux_comparison, np.sqrt(flux_comparison)])
+    
+    flux_1 = airmass + np.random.normal(0, stdev, 100)
+    auxiliary_star1 = np.transpose([t, flux_1, np.sqrt(flux_1)])
+    
+    flux_2 = airmass + np.random.normal(0, stdev, 100)
+    auxiliary_star2 = np.transpose([t, flux_2, np.sqrt(flux_2)])
+    
+    flux_3 = airmass + np.random.normal(0, stdev, 100)
+    auxiliary_star3 = np.transpose([t, flux_3, np.sqrt(flux_3)])
+    
     auxiliary_stars = np.array([auxiliary_star1, auxiliary_star2, auxiliary_star3])
-    student = IDP(target, comparison_star, auxiliary_stars)
+    student = IDP_w_stdev(target, comparison_star, auxiliary_stars)
     correlated_target, comparison_airmass, mean_aml = student.do() 
     #----------------------------------------
     # Plot the answers
@@ -47,7 +57,7 @@ if __name__ == "__main__":
     plt.ylabel('flux')
     frame1 = plt.gca()
     frame1.axes.get_xaxis().set_visible(False)
-    plt.scatter(target[:,0], target[:,1], label = 'observed_target')
+    plt.errorbar(target[:,0], target[:,1], yerr = target[:,2], fmt = 'ro', label = 'observed_target')
     plt.legend()
     
     plt.subplot(2, 1, 2)
@@ -55,16 +65,16 @@ if __name__ == "__main__":
     plt.ylabel('flux')
     plt.xlabel('time')
     plt.scatter(t, intrinsic, label = 'intrinsic_target', alpha = 0.5)
-    plt.scatter(correlated_target[:,0], correlated_target[:,1], label = 'correlated_target', alpha = 0.5)
+    plt.errorbar(correlated_target[:,0], correlated_target[:,1], yerr = correlated_target[:,2], fmt = 'ro', label = 'correlated_target', alpha = 0.5)
     plt.legend()
-
+    
     # comparison_airmass
     comparison_airmass_plt = plt.figure("comparison_airmass", figsize = (8, 3), dpi = 100)
     plt.title('comparison_airmass')
     plt.ylabel('flux')
     plt.xlabel('time')
     plt.scatter(t, airmass, label = 'intrinsic_airmass', alpha = 0.5)
-    plt.scatter(comparison_airmass[:,0], comparison_airmass[:,1], label = 'comparison_airmass', alpha = 0.5)
+    plt.errorbar(comparison_airmass[:,0], comparison_airmass[:,1], comparison_airmass[:,2], fmt = 'ro', label = 'comparison_airmass', alpha = 0.5)
     plt.legend()
     
     # mean of airmass less star 
@@ -72,7 +82,7 @@ if __name__ == "__main__":
     plt.title('mean of airmass less star')
     plt.ylabel('flux')
     plt.xlabel('time')
-    plt.scatter(mean_aml[:,0], mean_aml[:,1], label = 'mean of airmass less star')
+    plt.errorbar(mean_aml[:,0], mean_aml[:,1], yerr = mean_aml[:,2], fmt = 'ro', label = 'mean of airmass less star')
     plt.legend()
     plt.show()
 
